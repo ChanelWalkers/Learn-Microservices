65318434e284d88e0fc7c218319d8548b6b67c3c
ChanelWalkers <phungvantiendat@gmail.com>
2026-04-20 15:43:38 +0700

    [Add] Loans, Cards microservices

---
+++ section02/cards/src/main/java/com/tiendat/cards/dto/ResponseDTO.java
@@ -0,0 +1,13 @@
+package com.tiendat.cards.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseDTO {
+    private String statusCode;
+    private String statusMsg;
+}
